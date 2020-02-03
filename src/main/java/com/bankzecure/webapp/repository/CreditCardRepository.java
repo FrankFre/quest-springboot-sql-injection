a6084fdd79afc994ffa7ffb62bd988f7a85d9478
FrankFr <f.freisberg@telekom.de>
2020-02-03 21:28:54 +0100

    meine Solution

--- src/main/java/com/bankzecure/webapp/repository/CreditCardRepository.java
+++ src/main/java/com/bankzecure/webapp/repository/CreditCardRepository.java
@@ -1,8 +1,9 @@
 package com.bankzecure.webapp.repository;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -11,22 +12,31 @@ import com.bankzecure.webapp.entity.*;
 import com.bankzecure.webapp.JdbcUtils;
 
 public class CreditCardRepository {
-  private final static String DB_URL = "jdbc:mysql://localhost:3306/springboot_bankzecure?serverTimezone=GMT";
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/springboot_bankzecure?serverTimezone=GMT";
 	private final static String DB_USERNAME = "bankzecure";
 	private final static String DB_PASSWORD = "Ultr4B4nk@L0nd0n";
+	
+	
 
   public List<CreditCard> findByCustomerIdentifier(final String identifier) {
     Connection connection = null;
-    Statement statement = null;
+    
+//  Statement statement = null;
+    PreparedStatement preparedStatement = null;			
     ResultSet resultSet = null;
-    final String query = "SELECT cc.* FROM credit_card cc " +
-      "JOIN customer c ON cc.customer_id = c.id " +
-      "WHERE c.identifier = '" + identifier + "'";
+    
+//  final String query = "SELECT cc.* FROM credit_card cc " +						
+//  "JOIN customer c ON cc.customer_id = c.id " +
+//  "WHERE c.identifier = '" + identifier + "'";
+   
+    
     try {
       connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-      statement = connection.createStatement();
-      resultSet = statement.executeQuery(query);
-
+      preparedStatement = connection.prepareStatement("SELECT cc.* FROM credit_card cc JOIN customer c on cc.customer_id = c.id WHERE c.identifier = ?");
+      preparedStatement.setString(1, identifier);
+      resultSet = preparedStatement.executeQuery();
+      
+      
       final List<CreditCard> creditCards = new ArrayList<CreditCard>();
 
       while (resultSet.next()) {
@@ -44,7 +54,10 @@ public class CreditCardRepository {
       e.printStackTrace();
     } finally {
       JdbcUtils.closeResultSet(resultSet);
-      JdbcUtils.closeStatement(statement);
+      
+//    JdbcUtils.closeStatement(statement);
+      JdbcUtils.closeStatement(preparedStatement);
+      
       JdbcUtils.closeConnection(connection);
     }
     return null;
--- src/main/java/com/bankzecure/webapp/repository/CustomerRepository.java
+++ src/main/java/com/bankzecure/webapp/repository/CustomerRepository.java
@@ -1,28 +1,42 @@
 package com.bankzecure.webapp.repository;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.bankzecure.webapp.entity.*;
 import com.bankzecure.webapp.JdbcUtils;
 
 public class CustomerRepository {
-  private final static String DB_URL = "jdbc:mysql://localhost:3306/springboot_bankzecure?serverTimezone=GMT";
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/springboot_bankzecure?serverTimezone=GMT";
 	private final static String DB_USERNAME = "bankzecure";
 	private final static String DB_PASSWORD = "Ultr4B4nk@L0nd0n";
+	
 
   public Customer findByIdentifierAndPassword(final String identifier, final String password) {
     Connection connection = null;
-    Statement statement = null;
+    
+//  Statement statement = null;		
+    PreparedStatement preparedStatement = null;	
+    
     ResultSet resultSet = null;
+    
     try {
       connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-      statement = connection.createStatement();
-      final String query = "SELECT * FROM customer " +
-        "WHERE identifier = '" + identifier + "' AND password = '" + password + "'";
-      resultSet = statement.executeQuery(query);
+      
+//    statement = connection.createStatement();
+//    final String query = "SELECT * FROM customer " +
+//    "WHERE identifier = '" + identifier + "' AND password = '" + password + "'";
+     
+      preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE identifier = ? AND password = ?");      	
+      preparedStatement.setString(1, identifier);
+      preparedStatement.setString(2,  password);
+      
+//    resultSet = statement.executeQuery(query);
+      resultSet = preparedStatement.executeQuery();  
+      
 
       Customer customer = null;
 
@@ -39,7 +53,7 @@ public class CustomerRepository {
       e.printStackTrace();
     } finally {
       JdbcUtils.closeResultSet(resultSet);
-      JdbcUtils.closeStatement(statement);
+      JdbcUtils.closeStatement(preparedStatement);
       JdbcUtils.closeConnection(connection);
     }
     return null;
@@ -48,7 +62,10 @@ public class CustomerRepository {
   public Customer update(String identifier, String newEmail, String newPassword) {
 
     Connection connection = null;
-    Statement statement = null;
+    
+//  Statement statement = null;
+    PreparedStatement preparedStatement = null;
+    
     ResultSet resultSet = null;
     Customer customer = null;
     try {
@@ -56,26 +73,55 @@ public class CustomerRepository {
         connection = DriverManager.getConnection(
           DB_URL, DB_USERNAME, DB_PASSWORD
         );
-        statement = connection.createStatement();
+//      statement = connection.createStatement();
 
         // Build the update query using a QueryBuilder
         StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("UPDATE customer SET email = '" + newEmail + "'");
+
+//      queryBuilder.append("UPDATE customer SET email = '" + newEmail + "'");
+        queryBuilder.append("UPDATE customer SET email = ?");
+        
         // Don't set the password in the update query, if it's not provided
         if (newPassword != "") {
-          queryBuilder.append(",password = '" + newPassword + "'");
+        	
+//      queryBuilder.append(",password = '" + newPassword + "'");
+        queryBuilder.append(",password = ?");	
         }
-        queryBuilder.append(" WHERE identifier = '" + identifier + "'");
+        
+//      queryBuilder.append(" WHERE identifier = '" + identifier + "'");
+        queryBuilder.append(" WHERE identifier = ?");
+  
         String query = queryBuilder.toString();
-        statement.executeUpdate(query);
+        
+//      statement.executeUpdate(query);
+        preparedStatement = connection.prepareStatement(query);        
 
-        JdbcUtils.closeStatement(statement);
+        preparedStatement.setString(1, newEmail);
+        if (newPassword != "") {
+        	preparedStatement.setString(2, newPassword);
+        	preparedStatement.setString(3, identifier);
+		} else {
+			preparedStatement.setString(2, identifier);
+		}
+        
+        if (preparedStatement.executeUpdate() != 1) {
+        	throw new SQLException("failed to update data");
+		}
+        
+              
+        
+        JdbcUtils.closeStatement(preparedStatement);
         JdbcUtils.closeConnection(connection);
 
         connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-        statement = connection.createStatement();
-        query = "SELECT * FROM customer WHERE identifier = '" + identifier + "'";
-        resultSet = statement.executeQuery(query);
+        
+//      statement = connection.createStatement();
+//      query = "SELECT * FROM customer WHERE identifier = '" + identifier + "'";
+//      resultSet = statement.executeQuery(query);
+        
+        preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE identifier = ?");
+        preparedStatement.setString(1, identifier);
+        resultSet = preparedStatement.executeQuery();
 
         if (resultSet.next()) {
           final int id = resultSet.getInt("id");
@@ -89,7 +135,7 @@ public class CustomerRepository {
     } catch (SQLException e) {
         e.printStackTrace();
     } finally {
-   	    JdbcUtils.closeStatement(statement);
+   	    JdbcUtils.closeStatement(preparedStatement);
    	    JdbcUtils.closeConnection(connection);
     }
     return null;
